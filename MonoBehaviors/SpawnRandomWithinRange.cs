01586f0293911a1f7c26a407888195fe0120f1e7
ThetaRichPC <10991478@uvu.edu>
2024-02-22 23:04:54 -0700

    Improved enemy spawn and management system

--- MonoBehaviors/SpawnRandomWithinRange.cs
+++ MonoBehaviors/SpawnRandomWithinRange.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using UnityEngine;
 
-public class RandomSpawnWithinRange : MonoBehaviour
+public class SpawnRandomWithinRange : MonoBehaviour
 {
-    [SerializeField] private Vector3 spawnRange, deadZone, spawnOffset;
+    [SerializeField] private Vector3 spawnRange, spawnOffset;
     [SerializeField] private FloatData timeBetweenSpawns;
     [SerializeField] private BoolData continueSpawning;
     [SerializeField] private GameObject[] gameObjects;
@@ -16,9 +16,6 @@ public class RandomSpawnWithinRange : MonoBehaviour
         spawnRange.x = Mathf.Abs(spawnRange.x);
         spawnRange.y = Mathf.Abs(spawnRange.y);
         spawnRange.z = Mathf.Abs(spawnRange.z);
-        deadZone.x = Mathf.Abs(deadZone.x);
-        deadZone.y = Mathf.Abs(deadZone.y);
-        deadZone.z = Mathf.Abs(deadZone.z);
     }
 
     private void Start()
@@ -31,10 +28,8 @@ public class RandomSpawnWithinRange : MonoBehaviour
         while (continueSpawning.value)
         {
             yield return waitForSecondsObj;
-            int topOrBottom = -1 + Random.Range(0, 2)*2;
-            int leftOrRight = -1 + Random.Range(0, 2) * 2;
-            float positionX = transform.position.x + leftOrRight * Random.Range(deadZone.x, spawnRange.x) + spawnOffset.x;
-            float positionZ = transform.position.z + topOrBottom * Random.Range(deadZone.z, spawnRange.z) + spawnOffset.z;
+            float positionX = transform.position.x + Random.Range(-1f * spawnRange.x, spawnRange.x) + spawnOffset.x;
+            float positionZ = transform.position.z + Random.Range(-1f * spawnRange.z, spawnRange.z) + spawnOffset.z;
             Vector3 spawnPoint = new Vector3(positionX, transform.position.y + spawnOffset.y, positionZ);
             Instantiate(gameObjects[Random.Range(0, gameObjects.Length)], spawnPoint, transform.rotation);
         }
