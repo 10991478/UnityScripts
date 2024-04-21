dc1a2bda27e5f345bc6d5d0c571bfdb02fc9acde
ThetaRichPC <10991478@uvu.edu>
2024-04-21 11:37:33 -0600

    Upgraded snapping to work with float values not just int

--- MonoBehaviors/SnapToObjectPoints.cs
+++ MonoBehaviors/SnapToObjectPoints.cs
@@ -3,16 +3,17 @@ using UnityEngine;
 public class SnapToObjectPoints : MonoBehaviour
 {
     [SerializeField] private Transform otherTransform;
-    [SerializeField] private int snapScaleX = 1, snapScaleY = 1, snapScaleZ = 1;
-    private int posX, otherX, posY, otherY, posZ, otherZ;
+    [SerializeField] private float snapScaleX = 1, snapScaleY = 1, snapScaleZ = 1;
+    private float posX, otherX, posY, otherY, posZ, otherZ;
     void Update()
     {
-        otherX = (otherTransform.position.x >= 0) ? (int)(otherTransform.position.x + 0.5) : (int)(otherTransform.position.x - 0.5);
+        otherX = (otherTransform.position.x >= 0f) ? (otherTransform.position.x + snapScaleX * 0.5f) : (otherTransform.position.x - snapScaleX * 0.5f);
         posX = otherX - (otherX % snapScaleX);
-        otherY = (otherTransform.position.y >= 0) ? (int)(otherTransform.position.y + 0.5) : (int)(otherTransform.position.y - 0.5);
+        otherY = (otherTransform.position.y >= 0f) ? (otherTransform.position.y + snapScaleX * 0.5f) : (otherTransform.position.y - snapScaleX * 0.5f);
         posY = otherY - (otherY % snapScaleY);
-        otherZ = (otherTransform.position.z >= 0) ? (int)(otherTransform.position.z + 0.5) : (int)(otherTransform.position.z - 0.5);
+        otherZ = (otherTransform.position.z >= 0f) ? (otherTransform.position.z + snapScaleX * 0.5f) : (otherTransform.position.z - snapScaleX * 0.5f);
         posZ = otherZ - (otherZ % snapScaleZ);
+
         transform.position = new Vector3(posX, posY, posZ);
     }
 }
